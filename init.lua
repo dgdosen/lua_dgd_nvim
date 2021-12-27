026703c1cb42550ebfc990423b887e202259de46
Daniel Dosen <dgdosen@gmail.com>
2021-12-27 13:12:47 -0600

    wip: simplified plugin

--- init.lua
+++ init.lua
@@ -1,14 +1,7 @@
-print('Hello from lua_dgd_nvim')
-
 local function foo()
 
-  print("Hello from foo function")
-
-  local file = vim.fn.expand("%:p")
-  print ("file is " .. file)
-
   vim.cmd("vsplit | terminal")
-  local command = ':call jobsend(b:terminal_job_id, "echo hello world xxx\\n")'
+  local command = ':call jobsend(b:terminal_job_id, "echo hello world\\n")'
   vim.cmd(command)
 
 end
